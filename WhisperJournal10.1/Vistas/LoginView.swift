a57daffc27e64cf390ef642d9059b90cc1c220c2
Damian-GRT <andreelipa456@gmail.com>
2024-12-26 14:44:41 -0500

    ok

--- WhisperJournal10.1/Vistas/LoginView.swift
+++ WhisperJournal10.1/Vistas/LoginView.swift
@@ -2,68 +2,14 @@
 //  LoginView.swift
 //  WhisperJournal10.1
 //
-//  Created by andree on 20/12/24.
+//  Created by andree on 21/12/24.
 //
 
-import Foundation
 import SwiftUI
 
 struct LoginView: View {
-    @State private var username: String = ""
-    @State private var password: String = ""
-    @State private var isAuthenticated: Bool = false
-    @State private var showAlert: Bool = false
-
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Iniciar Sesión")
-                    .font(.largeTitle)
-                    .padding()
-
-                TextField("Nombre de usuario", text: $username)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-
-                SecureField("Contraseña", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-
-                Button(action: {
-                    authenticateUser()
-                }) {
-                    Text("Iniciar Sesión")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .font(.headline)
-                }
-                .padding()
-
-                Spacer()
-            }
-            .padding()
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Error"), message: Text("Nombre de usuario o contraseña incorrectos"), dismissButton: .default(Text("OK")))
-            }
-            .background(
-                NavigationLink(destination: ContentView(), isActive: $isAuthenticated) {
-                    EmptyView()
-                }
-            )
-        }
-    }
-
-    func authenticateUser() {
-        // Lógica de autenticación (esto es solo un ejemplo)
-        if username == "andree" && password == "123456" {
-            UserDefaults.standard.set(true, forKey: "isAuthenticated")
-            isAuthenticated = true
-        } else {
-            showAlert = true
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
