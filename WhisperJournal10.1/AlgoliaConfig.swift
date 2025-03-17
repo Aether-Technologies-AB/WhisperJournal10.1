97ddfdf613ebc00f54258a87c6c822f69c4232b0
Damian-GRT <andreelipa456@gmail.com>
2025-03-17 15:54:33 -0500

    ok

--- WhisperJournal10.1/AlgoliaConfig.swift
+++ WhisperJournal10.1/AlgoliaConfig.swift
@@ -12,10 +12,10 @@ import AlgoliaSearchClient
 
 
 struct AlgoliaConfig {
-    static let appID = ApplicationID(rawValue: "86B7PTFF9V")
-    static let apiKey = APIKey(rawValue: "de317a218a24f11e2ead16dc2b5e2a0e")  // Cambia a tu Admin API Key
-    static let searchAPIKey = APIKey(rawValue: "dbaef478e7cb46b7cc64693f90342038")  // Cambia a tu Search API Key
-    static let indexName = IndexName(rawValue: "Transcriptions")
+    static let appID = ApplicationID(rawValue: "")
+    static let apiKey = APIKey(rawValue: "")  // Cambia a tu Admin API Key
+    static let searchAPIKey = APIKey(rawValue: "")  // Cambia a tu Search API Key
+    static let indexName = IndexName(rawValue: "")
     
 
 }
